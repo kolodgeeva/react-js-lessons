d464f31f439cd2dbc55a0e12f822b087e3cc3145
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-02-02 12:07:29 +0200

    Add 1-4 lessons

---
+++ lessons/Lesson4.Props.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {	
+	render() {
+		let txt = this.props.txt
+		return (
+			<div>
+				<h1>{txt}</h1>
+			</div>
+			);
+	}
+}
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'this is the default txt'
+}
+
+export default App
+
---
+++ main.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './lessons/Lesson4.Props.js';
+ReactDOM.render(<App cat={5}/>, document.getElementById('app'));
