7e5be69dd79a0f25991d5708bc3d5e1fa3933294
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-02-04 11:20:54 +0200

    Lesson 5 was added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './lessons/Lesson4.Props.js';
+import App from './lessons/Lesson5.Refs.js';
 ReactDOM.render(<App cat={5}/>, document.getElementById('app'));
