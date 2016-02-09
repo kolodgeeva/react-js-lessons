3f21dab0676109f1ccc6e838671abc79af812719
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-02-09 11:00:01 +0200

    Add 10-11 Lessons

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './lessons/Lesson9.Updating.js';
+import App from './lessons/Lesson11.Composable.js';
 ReactDOM.render(<App val={0}/>, document.getElementById('app'));
